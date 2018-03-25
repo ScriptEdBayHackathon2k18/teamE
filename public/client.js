4856b3e414f546bea81e0d28dfd2a9c67439bfbe
Glitch (paper-turtle-explorers) <none>
2018-03-25 18:40:12 +0000

    🍯🌷 Checkpoint ./views/index.html:789197/488:789214/36 ./public/client.js:789214/777 ./public/style.css:789167/74:789197/38:789214/42

--- public/client.js
+++ public/client.js
@@ -1,1 +1,31 @@
-//
+//charater variables
+var charater;
+var health = 0;
+var food = 20;
+var skills = [];//Shield,smoke,damage,item. Shen has shield, Razzi has smoke
+//location variables
+var x = 0;
+var y = 0;
+//grid variables
+var grid = {};
+for (var i = 1; i <= 10; i ++){
+  
+}
+
+function randomInt(min,max){
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function chooseCha(character){
+  if (character === "Razzi"){
+    skills.push("smoke");
+  } else {
+    skills.push("shield");
+  }
+  if (randomInt(0,1) === 0){
+    skills.push("damage");
+  } else {
+    skills.push("item");
+  }
+}
+
